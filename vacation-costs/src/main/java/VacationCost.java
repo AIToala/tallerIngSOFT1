ec86aa163dba4302cc2d52451910a8b62a32a129
Andres Toala <andrestoala2013@gmail.com>
2023-06-07 19:48:24 -0500

    Finished additional requirements but didnt generate bugs

--- vacation-costs/src/main/java/VacationCost.java
+++ vacation-costs/src/main/java/VacationCost.java
@@ -40,6 +40,16 @@ public final class VacationCost {
     /**price for NY.
      */
     private final int priceNY = 600;
+    /**price for extra package all inclusive.
+     */
+    private final int priceAllInclusive = 200;
+    /**price for extra package adventure.
+     */
+    private final int priceAdventure = 150;
+    /**price for adventure package.
+     */
+    private final int priceSpaWellness = 100;
+
     /**time limit for promotion fee.
      */
     private final int timeLimitProm = 30;
@@ -57,6 +67,25 @@ public final class VacationCost {
     private VacationCost() {
         total = 0;
         subCost = 0;
+    }
+    /**Getter of all inclusive price.
+     * @return double
+     */
+    private int getPriceAllInclusive() {
+        return priceAllInclusive;
+    }
+
+    /**Getter of adventure price.
+     * @return double
+     */
+    private int getPriceAdventure() {
+        return priceAdventure;
+    }
+    /**Getter of all sa price.
+     * @return double
+     */
+    private int getPriceSpaWellness() {
+        return priceSpaWellness;
     }
     /**Getter of baseCost.
      * @return double
@@ -180,6 +209,21 @@ public final class VacationCost {
         total = subtotal;
         return total;
     }
+    //
+    private double calculatespecial1s(
+    final String allInclusive, final String adventure, final String spaWellness
+    ) {
+        if (allInclusive == "y") {
+            total += getPriceAllInclusive();
+        }
+        if (adventure == "y") {
+            total += getPriceAdventure();
+        }
+        if (spaWellness == "y") {
+            total += getPriceSpaWellness();
+        }
+        return total;
+    }
     /**Main program.
      * Vacation cost calculator
      * @param args
@@ -204,6 +248,21 @@ public final class VacationCost {
                 System.out.print("\ntime in days: ");
                 time = s.nextInt();
                 validation(dest, travelers, time);
+                vc.getCost(dest, travelers, time);
+                System.out.println("Do you wish to add "
+                + "special options to your vacation package? (y)");
+                if (s.nextLine().toLowerCase() == "y") {
+                    System.out.print("\n\nDo you wist to add an "
+                    + "all-inclusive package for $200 extra? (y): ");
+                    String special1 = s.nextLine();
+                    System.out.print("\nDo you wist to add an adventure "
+                    + "activities package for $150 extra? (y): ");
+                    String special2 = s.nextLine();
+                    System.out.print("\nDo you wist to add a spa and wellness"
+                    + " package for $100 extra? (y): ");
+                    String special3 = s.nextLine();
+                    vc.calculatespecial1s(special1, special2, special3);
+                }
             } catch (InputMismatchException ex) {
                 logger.log(null, "Input Mismatch");
             } catch (Exception ex) {
@@ -211,8 +270,8 @@ public final class VacationCost {
             } finally {
                 s.close();
             }
-            double total = vc.getCost(dest, travelers, time);
-            System.out.printf("Total cost: %.2f\n", total);
+            System.out.printf("Total cost: %.2f\n", vc.getTotal());
+            System.out.println("Thanks for your purchase...");
         }
     }
     /**Validation of variables using Scanner.
